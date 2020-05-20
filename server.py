e0143b02274183cf9b047fcee5d5e5d76f8d95e1
Rodrigo <rodrigo.pueblas@hotmail.com>
2020-05-20 19:09:46 +0200

    Adding permission to foxsamply

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 from os import environ
+import os
 from flask import Flask
+import stat
+os.chmod('foxsamply', stat.S_IEXEC)
 
 app = Flask(__name__)
 app.run(host= '0.0.0.0', port=environ.get('PORT'))
